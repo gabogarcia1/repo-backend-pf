ab5e001d9dd12a34c872a2bc804cfaeb450e10c6
Gabo <gabegarcia916@gmail.com>
2021-06-05 12:06:43 -0300

    se termino hasta el punto 21 del proyecto

--- server/server.js
+++ server/server.js
@@ -1,21 +1,36 @@
-const express =require('require');
-const app =express();
+const express = require("express");
+const app = express();
+require("./config/config");
 
-app.get('/',function(req,res){
-    res.json('GET usuarios');
-})
-app.post('/usuarios',function(req,res){
-    res.json('POST usuarios');
-})
-app.put('/usuarios/:id',function(req,res){
-    res.json('PUT usuarios');
-})
-app.delete('/usuarios.:id',function(req,res){
-    res.json('DELETE usuarios');
-})
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
+app.get("/usuarios", function (req, res) {
+  res.json("GET usuarios");
+});
+app.post("/usuarios", function (req, res) {
+  let body = req.body;
+  if (body.usuario === undefined) {
+    res.status(400).json({
+      ok: false,
+      message: "El usuario es necesario",
+    });
+  } else {
+    res.json({
+      body,
+    });
+  }
+});
+app.put("/usuarios/:id", function (req, res) {
+  res.json("PUT usuarios");
+});
+app.delete("/usuarios/:id", function (req, res) {
+  res.json("DELETE usuarios");
+});
 
-
-app.listen(3000,()=>{
-    console.log('Escuchando puerto 3000');
-})
+app.listen(process.env.PORT, () => {
+  console.log("Escuchando puerto ",process.env.PORT);
+});
