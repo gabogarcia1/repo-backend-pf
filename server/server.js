b88413c098bd6fedd1cbf10f2e416eba8f9d904c
Knifucrab <maurosbringas@gmail.com>
2021-06-07 18:21:03 -0300

    delete usuario cambiando estado y find usuario validado por estado

--- server/server.js
+++ server/server.js
@@ -10,6 +10,24 @@ app.use(
 
 app.get("/usuarios", function (req, res) {
   res.json("GET usuarios");
+  Usuario.find({ estado: true }, "nombre email role estado")
+    .limit(limite)
+    .skip(desde)
+    .exec((err, usuarios) => {
+      if (err) {
+        return res.status(400).json({
+          ok: false,
+          err,
+        });
+      }
+      Usuario.count({ estado: true }, (err, conteo) => {
+        res.json({
+          ok: true,
+          usuarios,
+          cantidad: conteo,
+        });
+      });
+    });
 });
 app.post("/usuarios", function (req, res) {
   let body = req.body;
@@ -28,9 +46,30 @@ app.put("/usuarios/:id", function (req, res) {
   res.json("PUT usuarios");
 });
 app.delete("/usuarios/:id", function (req, res) {
-  res.json("DELETE usuarios");
+  let id = req.params.id;
+  Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        err,
+      });
+    }
+    14;
+    if (!usuarioBorrado) {
+      return res.status(400).json({
+        ok: false,
+        err: {
+          message: "Usuario no encontrado",
+        },
+      });
+    }
+    res.json({
+      ok: true,
+      usuario: usuarioBorrado,
+    });
+  });
 });
 
 app.listen(process.env.PORT, () => {
-  console.log("Escuchando puerto ",process.env.PORT);
+  console.log("Escuchando puerto ", process.env.PORT);
 });
