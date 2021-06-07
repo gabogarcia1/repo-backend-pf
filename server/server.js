3c5c2bcfaf6a6f6b90802c5c7497978cd27e73f2
Gabo <gabegarcia916@gmail.com>
2021-06-06 21:31:10 -0300

    se termino hasta el punto 50

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 require("./config/config");
-
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
 
+app.use(require('./rutas/usuario')); 
 app.get("/usuarios", function (req, res) {
   res.json("GET usuarios");
 });
@@ -31,6 +32,20 @@ app.delete("/usuarios/:id", function (req, res) {
   res.json("DELETE usuarios");
 });
 
+mongoose.connect(
+  "mongodb://localhost:27017/test",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  },
+  (err, res) => {
+    if (err) throw err;
+    console.log("Base de datos Online");
+  }
+);
+
 app.listen(process.env.PORT, () => {
-  console.log("Escuchando puerto ",process.env.PORT);
+  console.log("Escuchando puerto ", process.env.PORT);
 });
